cc3282221d9b0ef35f2e208beabf44698997b22e
LeonardoMerelli <93209606+LeonardoMerelli@users.noreply.github.com>
2021-11-06 12:43:36 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,42 +1,57 @@
-package Es2;
+package Es1;
 
 import java.util.*;
 
-public class Main 
-{
-	
-	public static void main(String[] args) 
-	{
-		
+public class Main {
+
+	public static void main(String[] args) {
+
 		int mincrediti = 180;
-		CorsoLaurea[] listaCorsi = {
-	     new CorsoLaurea("Informatica",mincrediti), 
-		 new CorsoLaurea ("Meccanica",mincrediti),
-		 new CorsoLaurea("Biomedica",mincrediti),
-		 new CorsoLaurea("Elettronica",mincrediti)};
+		CorsoLaurea[] listaCorsi = { new CorsoLaurea("Informatica", mincrediti),
+				new CorsoLaurea("Meccanica", mincrediti), new CorsoLaurea("Biomedica", mincrediti),
+				new CorsoLaurea("Elettronica", mincrediti), new CorsoLaurea("Meccanica", mincrediti),
+				new CorsoLaurea("Gestionale", mincrediti), new CorsoLaurea("Civile", mincrediti) };
 		int input;
 		Scanner in = new Scanner(System.in);
-		do 
-		{
-			do 
-			{
+		do {
+			do {
 				System.out.println('\n' + "Benvenuto, quale corso di laurea in ingegneria ti interessa?");
 				System.out.println("1) Informatica");
 				System.out.println("2) Meccanica");
 				System.out.println("3) Biomedica");
 				System.out.println("4) Elettronica");
+				System.out.println("5) Meccanica");
+				System.out.println("6) Gestionale");
+				System.out.println("7) Civile");
 				System.out.println("0) Esci");
 				input = in.nextInt();
-			}while(input <= 0 && input >= 4);
-			
-			switch(input) 
-			{
-			case 1: listaCorsi[0].Menu("Informatica");break;
-			case 2: listaCorsi[1].Menu("Meccanica");break;
-			case 3: listaCorsi[2].Menu("Biomedica");break;
-			case 4: listaCorsi[3].Menu("Elettronica");break;
-			case 0:return;
+			} while (input <= 0 && input >= 7);
+
+			switch (input) {
+			case 1:
+				listaCorsi[0].Menu("Informatica");
+				break;
+			case 2:
+				listaCorsi[1].Menu("Meccanica");
+				break;
+			case 3:
+				listaCorsi[2].Menu("Biomedica");
+				break;
+			case 4:
+				listaCorsi[3].Menu("Elettronica");
+				break;
+			case 5:
+				listaCorsi[4].Menu("Meccanica");
+				break;
+			case 6:
+				listaCorsi[5].Menu("Elettronica");
+				break;
+			case 7:
+				listaCorsi[6].Menu("Elettronica");
+				break;
+			case 0:
+				return;
 			}
-		}while(true);
+		} while (true);
 	}
 }
